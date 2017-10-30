b8b5ff5a21860a504958e5d52ecc2d5fc56e0232
mandarinka <investormephi@gmail.com>
2017-10-30 17:28:27 +0300

    Overrided equals and hashCode

--- app/src/main/java/com/example/mandarinka/androidapp/BranchItem.java
+++ app/src/main/java/com/example/mandarinka/androidapp/BranchItem.java
@@ -92,5 +92,37 @@ public class BranchItem {
     public void setDistance(String distance) {
         this.distance = distance;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BranchItem that = (BranchItem) o;
+
+        if (address != null ? !address.equals(that.address) : that.address != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (location != null ? !location.equals(that.location) : that.location != null)
+            return false;
+        if (workMode != null ? !workMode.equals(that.workMode) : that.workMode != null)
+            return false;
+        if (phoneNumber != null ? !phoneNumber.equals(that.phoneNumber) : that.phoneNumber != null)
+            return false;
+        if (quality != null ? !quality.equals(that.quality) : that.quality != null) return false;
+        return distance != null ? distance.equals(that.distance) : that.distance == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = address != null ? address.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (location != null ? location.hashCode() : 0);
+        result = 31 * result + (workMode != null ? workMode.hashCode() : 0);
+        result = 31 * result + (phoneNumber != null ? phoneNumber.hashCode() : 0);
+        result = 31 * result + (quality != null ? quality.hashCode() : 0);
+        result = 31 * result + (distance != null ? distance.hashCode() : 0);
+        return result;
+    }
 }
 
--- app/src/main/java/com/example/mandarinka/androidapp/Location.java
+++ app/src/main/java/com/example/mandarinka/androidapp/Location.java
@@ -1,9 +1,5 @@
 package com.example.mandarinka.androidapp;
 
-/**
- * Created by mandarinka on 22.10.17.
- */
-
 public class Location {
     private Double latitube;
     private Double longitube;
@@ -30,5 +26,25 @@ public class Location {
 
     public void setLongitube(Double longitube) {
         this.longitube = longitube;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Location location = (Location) o;
+
+        if (latitube != null ? !latitube.equals(location.latitube) : location.latitube != null)
+            return false;
+        return longitube != null ? longitube.equals(location.longitube) : location.longitube == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = latitube != null ? latitube.hashCode() : 0;
+        result = 31 * result + (longitube != null ? longitube.hashCode() : 0);
+        return result;
     }
 }
